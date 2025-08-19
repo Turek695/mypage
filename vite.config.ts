28a3f0ffdada4e1d25eca7bebb856f55783b6d5c
turek695 <turowski.luk@gmail.com>
2025-08-19 18:36:24 +0200

    update vite config to include new page in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
-  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        metronom: resolve(__dirname, 'metronom.html')
+      }
+    }
+  },
+  server: {
+    open: '/index.html'
+  }
 });
